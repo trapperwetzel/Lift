ef11f8aec9e9a6eb85b330b201492f95071854b2
t_coding <trapperwetzel@gmail.com>
2025-03-26 17:41:25 -0500

    added cooldown to squat exercise

--- Lift/Assets/Scripts/SquatMiniGameTest.cs
+++ Lift/Assets/Scripts/SquatMiniGameTest.cs
@@ -7,9 +7,13 @@ using UnityEngine.UI;
 
 public class SquatMiniGameTest : MonoBehaviour
 {
+
     public Animator animator;
     public int Reps { get; private set; }
+    
     public TextMeshProUGUI repText;
+
+    [SerializeField] private CoolDown cooldown;
 
     void Start()
     {
@@ -22,20 +26,42 @@ public class SquatMiniGameTest : MonoBehaviour
 
     public void Update()
     {
+        DoSquat();
+    }
+
+    // Squat/Lift animation (testing for now) 
+
+    public void DoSquat()
+    {
+        if (cooldown.IsCoolingDown) return;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Space bar pressed");
             animator.SetTrigger("DoSquat");
+            cooldown.StartCoolDown();
             Reps++;
             UpdateText();
 
-
+            
         }
-    }
 
+    }
     private void UpdateText()
     {
         repText.text = "Reps Completed: " + Reps;
     }
+
+}
+
+[System.Serializable]
+public class CoolDown {
+    [SerializeField] private float cooldownTime;
+    private float _nextMovementTime;
+
+    public bool IsCoolingDown => Time.time < _nextMovementTime;
+
+    public void StartCoolDown() => _nextMovementTime = Time.time + cooldownTime;
+
 
 }
